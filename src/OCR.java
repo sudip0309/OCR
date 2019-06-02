d54b55c2eef14c079b747e04bf2b00e5475a03b0
saurav529 <saurav.bhandari@yahoo.com>
2019-06-02 08:55:06 +0545

    OCR Library

--- src/OCR.java
+++ src/OCR.java
@@ -1,6 +1,19 @@
+import java.io.*;
+import net.sourceforge.tess4j.*;
+
 public class OCR {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
-        System.out.println("GITHUB TEST");
+        File imageFile = new File("web/test-image.png");
+        Tesseract instance = new Tesseract();
+        instance.setLanguage("eng");
+
+        try {
+
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
